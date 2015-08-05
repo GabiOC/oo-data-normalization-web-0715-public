0b978adaeb0d417776e687d77c3655a4b919e6a8
GabiOC <oconnor.gabriele@gmail.com>
2015-08-05 14:53:15 -0400

    all done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 class Song
   # code goes here
+
+  attr_accessor :title, :artist
+
+  def initialize
+    @title
+  end
+
+  def serialize
+    formatted_title = @title.downcase.gsub(/\s/, "_")
+
+    open("./tmp/#{formatted_title}.txt", "w") do |f|
+      f.puts "#{artist.name} - #{@title}"
+    end
+  end
 end
